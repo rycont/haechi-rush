57b503d35f8dac510fcdb70ec8677569c356593d
정한 <35295182+rycont@users.noreply.github.com>
2022-08-04 10:47:32 +0000

    chore: add ts build

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-import { rush } from "./rush";
+// import { rush } from "./rush";
 
-rush(__dirname + "/src", {
-  swagger: {
-    path: "sw",
-  },
-  port: 5000,
-  address: "127.0.0.1",
-  cors: "*",
-});
+// rush(__dirname + "/src", {
+//   swagger: {
+//     path: "sw",
+//   },
+//   port: 5000,
+//   address: "127.0.0.1",
+//   cors: "*",
+// });
+export * from "./rush"
